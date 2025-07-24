5b7add6ac27f70956bc1c3f6beafc4d24eef0f4c
julioJar4 <juliojaraitape@hotmail.com>
2025-07-24 11:12:09 -0300

    first commit

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -2,7 +2,7 @@
   <a class="brand" href="{{ home_url('/') }}">
     {!! $siteName !!}
   </a>
-  <h1 class="text-amber-200">testetete</h1>
+  <h1 class="text-amber-200">testetetDe</h1>
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
